40d1a72f7dc31435bd6eb0d23a48867d8581dd7f
Marcin <75163408+surfingnerdinc@users.noreply.github.com>
2021-01-06 22:58:11 +0100

    prime.c
    Multithreading added. Time reduced to 1/2 initial time

--- prime.c
+++ prime.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+
+
 
 int main () {
 
+clock_t start, stop;
 
 int begin, end;
 
@@ -13,8 +19,11 @@ scanf("%d", &end);
 
 printf("Range is starting in %d, and finish at %d\n", begin, end); 
 
-
+start = clock();
 int i, j;
+
+omp_set_num_threads(4);
+#pragma omp parallel for
 
 for(i = begin; i <= end; i++) { 
  for(j=2; j<=i; j++){
@@ -26,6 +35,16 @@ for(i = begin; i <= end; i++) {
   printf("Yup for %d\n", i); 
 }
 }
+
+stop = clock();
+
+
+printf("Start: %d\n", start);
+printf("Stop: %d\n", stop);
+
+printf("czas przetwarzania wynosi %f sekund\n", ((double)(stop-start) / 1000.0));
+printf("Czas przetwarzania w minutach wynosi %f min \n", ((double)(stop-start) / 1000.0 / 60));
+
 
 return 0;
 }
