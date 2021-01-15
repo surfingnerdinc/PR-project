9dcd044bf178f97cc26333d406fbf7befa9ac128
Marcin <75163408+surfingnerdinc@users.noreply.github.com>
2021-01-15 20:11:44 +0100

    Add files via upload
    Small changes...

--- Source.c
+++ Source.c
@@ -10,19 +10,16 @@
 
 int main(int argc, char* argv[]) {
 	
-	//moje
+	//Deklarowanie zmiennych czasowych do obliczenia czasu dzialania programu. 
 	clock_t start, stop;
 	
-
-	
+	//Proces wejsciowy. Pytanie o zakres, ilosc watkow, ktore chcemy uzyc. 
 	int beginOfRange, endOfRange;
 	printf("Set begin of range \n");
 	scanf("%d", &beginOfRange);
 
-
 	printf("Set end of range \n");
 	scanf("%d", &endOfRange);
-
 
 	int THREADS;
 	printf("Set amount of threads (1-4)\n");
@@ -38,42 +35,33 @@ int main(int argc, char* argv[]) {
 			break;
 		case 4: THREADS = 4;
 			break;
-		default: printf("errpr\n");
+		default: printf("ERROR. Available max 4 threads... \n");
+			return EXIT_FAILURE;
 			break;
 	}
 
-
+	//deklarowanie zmiennych. 
 	int mysqrt = (int)sqrt(endOfRange);
 	int matrixColumn = 256;
 	int matrixRow = (endOfRange / matrixColumn) + 1;
-	int end_of_range = 0;
-	int tmpCounter = 0;
 	int tabIndex = 0;
-	int matrix_currentColumn = 0;
-	int matrix_currentRow = 0;
-	int i, j = 0;
-	int col_index = 0, row_index = 0;
-	int temp_primeCounter = 0;
-	int primeCounter = 0;
+	int i =0, j = 0;
+	
+	
+	//Deklaracja tablicy z dzielnikami, ktora bedzie uzywana do pozniejszego wykreslania liczb
 	int* divisors;
+	divisors = (int*)calloc((mysqrt + 1), sizeof(int));
+
+
+	//inicjalizacja macierzy u�ywanej podczas wykre�lania liczb pierwszych
 	bool** divTab;
-
-
-
-				//inicjalizacja macierzy u�ywanej podczas wykre�lania liczb pierwszych
 	divTab = malloc((matrixColumn + 1) * sizeof(bool*));
 	for (int i = 0; i <= matrixColumn; ++i) {
 		divTab[i] = malloc((matrixRow + 1) * sizeof(bool));
 	}
 
 
-	//inicjalizacja tablicy z podzielnikami
-	divisors = (int*)calloc((mysqrt + 1), sizeof(int));
-
-
-	//zerowanie czasu rozpocz�cia i zko�czenia
-	//ustalanie ilo�ci w�tk�w
-	//czas rozpocz�cia wykonywania programu
+	//Rozpoczecie pomiaru czasu.
 	start = clock();
 
 	//szukanie liczb pierwszych w zakresie od 2..sqrt(n)
@@ -108,7 +96,8 @@ int main(int argc, char* argv[]) {
 	}
 
 
-	//wykre�lanie liczb, liczbami parzystymi
+	//Wykreslanie liczb z tablicy za pomoca liczb parzystych. 
+	int col_index = 0, row_index = 0, end_of_range = 0;
 	end_of_range = endOfRange / 2;
 #pragma omp parallel for shared(divTab,end_of_range) private(col_index, row_index, i) schedule(static,32)
 	for (i = 2; i <= end_of_range; i++) {
@@ -117,7 +106,7 @@ int main(int argc, char* argv[]) {
 		divTab[col_index][row_index] = false;
 	}
 
-	//wykre�lanie pozosta�ych liczb, liczbami pierwszych
+	//Wykreslanie liczb z tablicy (tych, ktore tutaj pozostaly) za pomoca liczb pierwszych.
 #pragma omp parallel for private(col_index, row_index,i,j,end_of_range) schedule(dynamic,1) shared(divTab, tabIndex)
 	for (i = 1; i < tabIndex; i++) {
 		end_of_range = endOfRange / divisors[i];
@@ -128,7 +117,10 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	//wypisywanie - zlicznie pierwszych
+
+	//Liczenie ile udało się uzyskać liczb pierwszych 
+	int temp_primeCounter = 0;
+	int primeCounter = 0;
 #pragma omp parallel for shared(divTab, matrixRow, matrixColumn) private(i,j) reduction(+: temp_primeCounter)
 	for (i = 0; i < matrixRow; i++) {
 		for (j = 0; j < matrixColumn; j++)
@@ -138,32 +130,45 @@ int main(int argc, char* argv[]) {
 			}
 		}
 	}
+
 	//Zakonczenie przetwarzania.
 	stop = clock();
 
 	primeCounter = temp_primeCounter;
-	printf("Prime counter: %d, App was running at: %fs. Was used: %d thread(s)\n", primeCounter, ((double)(stop-start) / 1000.0), THREADS);
+	printf("Prime counter: %d, App was running: %fs. Was used: %d thread(s)\n", primeCounter, ((double)(stop-start) / 1000.0), THREADS);
 
 	int save_to_xls;
-	printf("Do you want save and see prime numbers?\n");
+	printf("\n\nDo you want save and see prime numbers?\n");
 	printf("Type 1 for yest \n");
 	scanf("%d", &save_to_xls);
 
-	if (save_to_xls == 1) {
-		FILE* f = fopen("primeCounter.xls", "w");
-		fprintf(f, "Prime counter: %d, App was running at: %fs. Was used: %d thread(s)\n", primeCounter, ((double)(stop - start) / 1000.0), THREADS);
+
+
+	//Zmienne potrzebne do zapisywanie wynikow do pliku xls. 
+	FILE* excel_file;
+	char filename[17] = "primeCounter.xls\0";
+	int temporary = 0;
+
+	switch (save_to_xls)
+	{
+	case 1: 
+		excel_file = fopen(filename, "w"); //flaga W otwiera plik w trybie nadpisywania - jednoczesnie usuwajac stare dane. 
+		fprintf(excel_file, "Prime counter: %d, App was running at: %fs. Was used: %d thread(s)\n", primeCounter, ((double)(stop - start) / 1000.0), THREADS);
 		for (i = 0; i < matrixRow; i++) {
 			for (j = 0; j < matrixColumn; j++) {
 				if (divTab[j][i] == 1) {
-					if (tmpCounter % 10 == 0)fprintf(f, "\n");
-					fprintf(f, "%d, \t", matrixColumn * i + j);
-					tmpCounter++;
+					if (temporary % 10 == 0)fprintf(excel_file, "\n");
+					fprintf(excel_file, "%d, \t", matrixColumn * i + j);
+					temporary++;
 				}
 			}
 		}
-		fclose(f);
+		printf("Successfully saved to %s\n", filename);
+		break;
+	default:
+		return EXIT_SUCCESS;
+		break;
 	}
-
 
 	return 0;
 }
